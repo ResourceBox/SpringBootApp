dafeee58fbb753b3f24905487b6e61578abbc98f
resource <test@test.ru>
2017-09-21 17:36:15 +0300

    add project pattern

---
+++ src/main/java/ru/resource/app/SpringBootApp.java
@@ -0,0 +1,12 @@
+package ru.resource.app;
+
+/**
+ * Created by Resource on 21.09.2017.
+ */
+public class SpringBootApp {
+
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+
+}
